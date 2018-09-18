e193ba2d35c581ad8c820b2a609c803d09b1b054
guodongxiaren <879231132@qq.com>
2018-09-19 00:24:59 +0800

    ss

--- zk_dts/zk_dts/src/main/java/demo/zk_dts/App.java
+++ zk_dts/zk_dts/src/main/java/demo/zk_dts/App.java
@@ -1,13 +1,36 @@
 package demo.zk_dts;
 
-/**
- * Hello world!
- *
- */
+import java.util.List;
+
+import org.apache.zookeeper.ZooKeeper;
+
+ 
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+      	ZooKeeper zk = null;
+        
+        try {
+			zk = new ZooKeeper("127.0.0.1:2181", 500000, null);
+		    List<String> ls = zk.getChildren("/home", new ChildChangeWatcher("/home", zk));
+			for (String s:ls) {
+				System.out.println(s);
+			}
+
+			Thread.sleep(50000);
+			
+        } catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (zk != null) {
+				try {
+					zk.close();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}
     }
 }
